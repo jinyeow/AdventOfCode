020131e80dfbf87d5bf83ef936b9854a4fa7d0e6
Justin Puah <justin@puah.dev>
2025-12-03 00:20:47 +1100

    Add 2025 tests for Day01

---
+++ 2025.Tests/Day01.Tests.cs
@@ -0,0 +1,50 @@
+﻿using AdventOfCode2025.Days;
+
+namespace AdventOfCode2025.Tests;
+
+public class Day01Tests
+{
+    [Fact]
+    public void Part1_WithExampleInput_ReturnsExpectedResult()
+    {
+        // Arrange
+        string input = @"L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82";
+
+        // Act
+        int result = Day01.Part1(input);
+
+        // Assert
+        Assert.Equal(3, result);
+    }
+
+    [Fact]
+    public void Part2_WithExampleInput_ReturnsExpectedResult()
+    {
+        // Arrange
+        string input = @"L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82";
+
+        // Act
+        int result = Day01.Part2(input);
+
+        // Assert
+        Assert.Equal(6, result);
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Advent Of Code
+
+## Overview
+
+Advent of Code solutions in different languages for different years.
+
+## 2025 - C#
