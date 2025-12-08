8d0f61c21b6b13f9faba4503b4141b271ab1d3a8
Justin Puah <justin@puah.dev>
2025-12-08 22:54:21 +1100

    Initial logic for Day02 Part1 solution

--- 2025/Days/Day02/Day02.cs
+++ 2025/Days/Day02/Day02.cs
@@ -14,7 +14,35 @@ public static class Day02
 
     public static int Part1(string input)
     {
-        // TODO
+        var total = 0;
+        var line = input.Split('\n', StringSplitOptions.RemoveEmptyEntries);
+        var ranges = line.Split(',');
+
+        foreach (var (firstValue, lastValue) in ranges.Split('-'))
+        {
+            var start = int.Parse(firstValue);
+            var end = int.Parse(lastValue);
+
+            if (firstValue.length % 2 == 0)
+            {
+                var sequenceLength = firstValue.length / 2;
+            }
+            else if (lastValue.length % 2 != 0)
+            {
+                var sequenceLength = lastValue.length / 2;
+            }
+            else
+            {
+                continue;
+            }
+
+            // check if the last sequenceLength digits of the lastValue are smaller than the first sequenceLength digits of the firstValue
+            // if not, continue
+            // find all the numbers where the first sL == last sL digits
+
+            var invalidId = 0;
+            total += invalidId;
+        }
     }
 
     public static int Part2(string input)
